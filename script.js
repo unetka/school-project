2c3665682d63cdf02790e7f669f52c990f238145
unetka <99989935+unetka@users.noreply.github.com>
2025-11-26 22:50:26 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let mainblock = document.querySelector("#main-block");
 let i = document.getElementById('main-block').style.top;
 navbutton.addEventListener('click', function(){
             document.getElementById('main-block').style.right = '0%';
-            window.scrollBy({left: 0, top: 1200, behavior: 'smooth' });
+            mainblock.scrollIntoView({behavior: "smooth"});
 });
